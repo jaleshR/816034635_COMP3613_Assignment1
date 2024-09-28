2ee994dc129c2331392e307c8f16f8c7902be8af
jalesh Ramkhalawan <jalesh.ramkhalawan@my.uwi.edu>
2024-09-27 21:59:13 -0400

    Update result.py

--- App/controllers/result.py
+++ App/controllers/result.py
@@ -1,15 +1,13 @@
-# App/controllers/result.py
 
 import csv
 from App.models.result import Result
 from App.database import db
 
-class ResultController:
-    def import_results(file_path, competition_id):
-        with open(file_path, 'r') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                result = Result(user_id=row['user_id'], competition_id=competition_id,
-                                score=row['score'], ranking=row['ranking'])
-                db.session.add(result)
-            db.session.commit()
+def import_results(file_path, competition_id):
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            result = Result(user_id=row['user_id'], competition_id=competition_id,
+                            score=row['score'], ranking=row['ranking'])
+            db.session.add(result)
+        db.session.commit()
